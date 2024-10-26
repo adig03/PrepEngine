fb1091cdefc50758c20717ebdab487dd8e09751b
adig03 <149831123+adig03@users.noreply.github.com>
2024-10-26 22:27:58 +0530

    13th Commit  -> auth.currentUser issue rectified

--- app/src/main/java/com/example/codemastery/intro/IntroActivity.kt
+++ app/src/main/java/com/example/codemastery/intro/IntroActivity.kt
@@ -1,6 +1,7 @@
 package com.example.codemastery.intro
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,17 +10,28 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.codemastery.R
+import com.example.codemastery.main.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var auth :FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
-             navController = navHostFragment.navController
 
 
+auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser!= null){
+            val i  =    Intent(this , MainActivity::class.java)
+            startActivity(i)
+        }
+        else{
+            setContentView(R.layout.activity_intro)
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+            navController = navHostFragment.navController
+        }
 //        val sharedPref = getSharedPreferences("appPreferences", Context.MODE_PRIVATE)
 //        val editor = sharedPref.edit()
 //        editor.putBoolean("isFirstLaunch", false)
